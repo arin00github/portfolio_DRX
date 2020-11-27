bf6ab30222f57b08a260b8d1e666b0ab91414daa
leegin <teru325@naver.com>
2020-11-27 12:07:30 +0900

    responsive make

--- js/main.js
+++ js/main.js
@@ -86,6 +86,18 @@ $('#news-slider').slick({
             settings:{
                 slidesToShow : 3,
             }
+        },
+        {
+            breakpoint:768,
+            settings:{
+                slidesToShow : 2,
+            }
+        },
+        {
+            breakpoint:540,
+            settings:{
+                slidesToShow : 1,
+            }
         }
 
     ]
